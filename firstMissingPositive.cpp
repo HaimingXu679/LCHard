e7ea80b7782a4d6eee1155262a68d9f706b9548d
Haiming Xu <35410477+HaimingXu679@users.noreply.github.com>
2020-05-29 20:43:21 -0400

    Update firstMissingPositive.cpp

--- firstMissingPositive.cpp
+++ firstMissingPositive.cpp
@@ -1,15 +1,18 @@
 // https://leetcode.com/problems/first-missing-positive/
 
+// Mathematics solution; ad-hoc
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int i,j;
-        int *n=new int[nums.size()+1];
-        for (i=0;i<nums.size()+1;i++) n[i]=1;
-        for (i=0;i<nums.size();i++)
-            if (nums[i]<=nums.size()+1 && nums[i]>0)
-                n[nums[i]-1]=-100000000;
-        for (i=0;i<nums.size()+1;i++) cout << n[i] << endl;
-        for (i=0;i<nums.size()+1;i++) if (n[i]!=-100000000) return i+1;
+        int *n = new int[nums.size() + 1];
+        for (int i = 0; i < nums.size() + 1; i++) 
+            n[i] = 1;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] <= nums.size() + 1 && nums[i] > 0)
+                n[nums[i] - 1] = -100000000;
+        for (int i = 0; i < nums.size() + 1; i++) 
+            if (n[i] != -100000000) 
+                return i + 1;
     }
 };
