832094d9fd6b1bdaab47adc36bb9485915daa73f
Haiming Xu <35410477+HaimingXu679@users.noreply.github.com>
2020-07-27 17:33:44 -0400

    Create splitArrayLargestSum.cpp

---
+++ splitArrayLargestSum.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/split-array-largest-sum/
+
+// Dynamic programming
+
+// O(N^3), beats 26%
+
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        long long dp[nums.size()][m], accumulator = 0, postfix[nums.size()];
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            postfix[i] = accumulator += nums[i];
+            for (int j = 0; j < m; j++)
+                dp[i][j] = j == 0 ? accumulator : 0x7fffffff;
+        }
+        for (int i = nums.size() - 2; i >= 0; i--)
+            for (int j = 1; j < m; j++)
+                for (int k = i + 1; k < nums.size(); k++)
+                    dp[i][j] = min(dp[i][j], max(postfix[i] - postfix[k], dp[k][j - 1]));
+        return dp[0][m - 1];
+    }
+};
